83a59ba46d2185334dbd12be3fef966b7facf374
Osmari13 <osmaribarriga@gmail.com>
2026-03-28 15:05:44 -0400

    fix prisma 2

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,7 +9,7 @@ export const prisma =
     // a veces necesitas pasar la URL aquí explícitamente si no lee el config
     datasources: {
       db: {
-        url: process.env.DATABASE_URL,
+        url: process.env.DATABASE_URL_DATABASE_URL || process.env.DATABASE_URL, 
       },
     },
   })
